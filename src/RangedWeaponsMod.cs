3a77ee1c4020b932105fb4ac909541e72b5c4054
Zarkonnen <david.stark@zarkonnen.com>
2023-04-16 16:28:04 +0200

    Attempt at autoadding poisonable.

--- src/RangedWeaponsMod.cs
+++ src/RangedWeaponsMod.cs
@@ -1,6 +1,7 @@
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Server;
+using Vintagestory.API.Common.Entities;
 
 [assembly: ModInfo( "RangedWeapons",
 	Description = "Implements a more flexible ranged weapons system.",
@@ -18,7 +19,20 @@ namespace RangedWeapons
 			api.RegisterItemClass("ItemPoisonArrow", typeof(ItemPoisonArrow));
 			api.RegisterEntity("EntityPoisonProjectile", typeof(EntityPoisonProjectile));
 			api.RegisterEntityBehaviorClass("poisonable", typeof(Poisonable));
+			//api.Event.OnEntitySpawn += AppendEntityBehaviors;
 		}
+
+		// Would be a cool way to do it but doesn't work on already existing animals.
+		/*
+		public void AppendEntityBehaviors(Entity entity) {
+			if (entity.Code.ToString().ToLower().Contains("drifter")) return;
+			if (entity.Code.ToString().ToLower().Equals("bell")) return;
+			if (entity.Code.ToString().ToLower().Contains("locust")) return;
+			if (entity.Code.ToString().ToLower().Contains("dummy")) return;
+			if (entity.Code.ToString().ToLower().Contains("dead")) return;
+			entity.AddBehavior(new Poisonable(entity));
+		}
+		*/
 		
 		public override void StartClientSide(ICoreClientAPI api)
 		{
